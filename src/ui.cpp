08fc581a8336fb65ee3a2c79d99e226968cb9df0
ccooper1982@proton.me <ccooper1982@proton.me>
2025-03-19 18:35:10 +0000

    Qt less broken

--- src/ui.cpp
+++ src/ui.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <map>
 #include <qt/QtWidgets/QApplication>
 #include <qt/QtWidgets/QLabel>
 #include <qt/QtWidgets/QFormLayout>
@@ -12,63 +13,126 @@
 #include <qt/QtWidgets/QMainWindow>
 
 
+struct ContentWidget : public QWidget
+{
+  ContentWidget(const QString& nav_name) : m_nav_name(nav_name)
+  {
+    
+  }
+
+
+  virtual const QString get_nav_name() const
+  {
+    return m_nav_name;
+  }
+
+
+private:
+  QString m_nav_name;
+};
+
+
+struct WelcomeWidget : public ContentWidget
+{
+  WelcomeWidget(const QString& nav_name) : ContentWidget(nav_name)
+  {
+    QVBoxLayout * layout = new QVBoxLayout;
+    layout->setAlignment(Qt::AlignHCenter | Qt::AlignTop);
+
+    QLabel * label = new QLabel("Welcome");
+    layout->addWidget(label);
+
+    setLayout(layout);
+  }
+};
+
+
+struct NetworkWidget : public ContentWidget
+{
+  NetworkWidget(const QString& nav_name) : ContentWidget(nav_name)
+  {
+    QFormLayout * network_layout = new QFormLayout;
+    QLineEdit * ledit_hostname = new QLineEdit;
+    network_layout->addRow("Hostname: ", ledit_hostname);
+
+    setLayout(network_layout);
+  }
+};
+
+
 struct NavTree : public QTreeView
 {
-  NavTree()
+private:
+  const std::vector<ContentWidget*> NavItemToWidget = 
+  {
+    new WelcomeWidget{"Welcome"},
+    new NetworkWidget{"Network"}
+    // Locales, Partitions, Bootloader, Accounts, Profile, Audio, Video, Packages
+  };
+
+public:
+
+  explicit NavTree(QLayout * const central_layout) : m_central_layout(central_layout)
   {
     m_model = new QStandardItemModel{0,0};
 
-    QStandardItem * item_locale = new QStandardItem("Locale");
-    QStandardItem * item_partitions = new QStandardItem("Partitions");
-    QStandardItem * item_kernels = new QStandardItem("Kernels");
-    QStandardItem * item_network = new QStandardItem("Network");
-    QStandardItem * item_accounts = new QStandardItem("Accounts");
-    QStandardItem * item_video = new QStandardItem("Video");
-    QStandardItem * item_audio = new QStandardItem("Audio");
-    QStandardItem * item_profiles = new QStandardItem("Profiles");
-    QStandardItem * item_packages = new QStandardItem("Packages");
-
-
-    m_model->appendRow(item_locale);      // keymap, timezone, NTP
-    m_model->appendRow(item_partitions);  // /boot, /, /home, swap
-    m_model->appendRow(item_kernels);     // linux, linux-lts, linux-zen
-    m_model->appendRow(item_accounts);    // set root passwd, create user account
-    m_model->appendRow(item_network);     // hostname, copy config to install
-    m_model->appendRow(item_video);
-    m_model->appendRow(item_audio);
-    m_model->appendRow(item_profiles);    // no desktop, with desktop+select WM and DE
-    m_model->appendRow(item_packages);    // additional packages (standard: base, linux-firmware, <kernels>, <gpu>)
+    for (const auto item : NavItemToWidget)
+      m_model->appendRow(new QStandardItem(item->get_nav_name()));
 
     setModel(m_model);
     setHeaderHidden(true);
     setMaximumWidth(200);
-
     setSelectionMode(QAbstractItemView::SelectionMode::SingleSelection);
     setSelectionBehavior(QAbstractItemView::SelectionBehavior::SelectRows);
 
     connect(selectionModel(), &QItemSelectionModel::selectionChanged, this, [this](const QItemSelection& selected, const QItemSelection& deselected)
     {
-      QModelIndexList selectedIndexes = selected.indexes(); 
-      if (!selectedIndexes.isEmpty())
+      if (const QModelIndexList selectedIndexes = selected.indexes() ; !selectedIndexes.isEmpty())
       {
-          QModelIndex index = selectedIndexes.first(); 
-          QVariant data = m_model->data(index, Qt::DisplayRole); 
-          qDebug() << "Selected item:" << data.toString(); 
+        const QModelIndex index = selectedIndexes.first(); 
+        const QVariant data = m_model->data(index, Qt::DisplayRole);         
+
+        if (index.row() >= 0 && index.row() < std::ssize(NavItemToWidget))
+        {
+          auto next = NavItemToWidget[index.row()];
+
+          if (m_current)
+          {
+            m_current->hide();
+            m_central_layout->replaceWidget(m_current, next, Qt::FindDirectChildrenOnly);
+          }
+          else
+          {
+            m_central_layout->addWidget(next);
+          }
+
+          m_current = next;
+          m_current->show();
+        }
       }
     });
   }
 
-  private:
-    QStandardItemModel * m_model;
+  ContentWidget * get_welcome_widget()
+  {
+    return NavItemToWidget[0];
+  }
+
+  void show_welcome()
+  {
+    //QItemSelection s(1);
+    //selectionModel()->select(s, QItemSelectionModel::ClearAndSelect);
+    setCurrentIndex(model()->index(0,0));
+  }
+
+private:
+  QLayout * m_central_layout;
+  QWidget * m_current{nullptr};
+  QStandardItemModel * m_model;
+  QModelIndex m_welcome_index;
 };
 
 
-QTreeView * create_nav_tree()
-{
-  NavTree * tree = new NavTree;
-  
-  return tree;
-}
 
 
 int main (int argc, char ** argv)
@@ -83,14 +147,14 @@ int main (int argc, char ** argv)
 
   QApplication app(argc, argv);
   
+
   QHBoxLayout * central_layout = new QHBoxLayout;
   QVBoxLayout * nav_layout = new QVBoxLayout;
-  QWidget * wgt_content = new QWidget;
- 
 
-  central_layout->addLayout(nav_layout);
-  central_layout->addWidget(wgt_content, 1);
+  NavTree * nav_tree = new NavTree(central_layout);
   
+  central_layout->addLayout(nav_layout);
+  //central_layout->addWidget(content_widget, 1); 
   
   QWidget * wgt_central = new QWidget;
   wgt_central->setLayout(central_layout);
@@ -106,23 +170,13 @@ int main (int argc, char ** argv)
   // wgt_content->setAutoFillBackground(true);
   // wgt_content->setPalette(pal);
 
-  nav_layout->addWidget(create_nav_tree());
-
-  // QPushButton * btn_partitions = new QPushButton("Partitions");
-  // QPushButton * btn_network = new QPushButton("Network");
-  // nav_layout->addWidget(btn_partitions, Qt::AlignHCenter);
-  // nav_layout->addWidget(btn_network, Qt::AlignHCenter);
+  nav_layout->addWidget(nav_tree);
 
   // content
-  central_layout->addWidget(wgt_content);
+  //central_layout->addWidget(wgt_content);
 
-  //
-  QFormLayout * network_layout = new QFormLayout;
-  QLineEdit * ledit_hostname = new QLineEdit;
-  network_layout->addRow("Hostname: ", ledit_hostname);
-  
-  //
-  central_layout->addLayout(network_layout);
+  // always show Welcome initially
+  nav_tree->show_welcome();
 
   window.show();
   return app.exec();
