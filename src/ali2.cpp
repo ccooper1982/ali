84e4a2a88c703afca68b6885bcfeb85ee094e25d
ccooper1982@proton.me <ccooper1982@proton.me>
2025-05-03 23:14:56 +0100

    Playing with ftxui events and components

--- src/ali2.cpp
+++ src/ali2.cpp
@@ -11,6 +11,11 @@
 
 using namespace ftxui;
 
+
+std::string debug;
+
+
+
 static const std::string Welcome_Waffle = "Some introductory waffle";
 
 std::vector<std::string> keymaps = {" -- ", "gb", "fr", "de"};
@@ -21,6 +26,65 @@ int locale_selected{0};
 
 std::vector<std::string> timezones = {" -- ", "Europe/London", "America/New York"};
 int timezone_selected{0};
+
+std::vector<std::string> devices = {" -- ", "/dev/sda1", "/dev/sda2"};
+
+std::vector<std::string> parts1 = {"/dev/sda1p1", "/dev/sda1p1"};
+std::vector<std::string> parts2 = {"/dev/sda2p1", "/dev/sda2p1"};
+std::vector<std::string> parts;
+
+struct Mounts : public ComponentBase
+{
+  Mounts()
+  {
+    devices_dd = Dropdown(&devices, &m_selected_device) | size(WIDTH, EQUAL, 25);
+    devices_dd |= CatchEvent([this](Event evt)
+    {
+      if (m_selected_device == 1)
+        parts = parts1;
+      else if (m_selected_device == 2)
+        parts = parts2;
+      else
+        parts.clear();
+      
+      return false;
+    });
+
+    auto dev = Container::Horizontal(
+    {
+      Renderer([]{ return text("Device") | vcenter | size(WIDTH, EQUAL, 15) ; }),
+      devices_dd
+    });
+
+
+    partitions_dd = Dropdown(&parts, &m_selected_part) | size(WIDTH, EQUAL, 25);
+
+    partitions = Maybe(Container::Horizontal(
+    {
+      Renderer([]{ return text("Partitions") | vcenter | size(WIDTH, EQUAL, 15) ; }),
+      partitions_dd
+    }), [this]{ return m_selected_device != 0;}) ;
+
+
+    auto layout = Container::Vertical({dev, partitions});
+
+    Add(layout);
+  }
+
+  virtual bool OnEvent(Event evt) override
+  {
+    //debug = "Mounts::OnEvent";
+    return ComponentBase::OnEvent(evt); 
+  }
+
+private:
+  Component devices_dd;
+  Component partitions_dd;
+  Component partitions;
+  int m_selected_device{0};
+  int m_selected_part{0};
+};
+
 
 
 int main (int argc, char ** argv)
@@ -35,25 +99,23 @@ int main (int argc, char ** argv)
   int tab_selected = 0;
   auto tab_toggle = Toggle(&tab_values, &tab_selected);
 
-  //int tab_1_selected = 0;
-  int tab_2_selected = 0;
-  int tab_3_selected = 0;
+  auto mounts = ftxui::Make<Mounts>();
 
-  std::vector<std::string> tab_1_entries{"Forest","Water","I don't know"};
-  std::vector<std::string> tab_2_entries{"Hello","Hi","Hay"};
-  std::vector<std::string> tab_3_entries{"Table","Nothing","Is","Empty"};
-  
   auto tab_container = Container::Tab(
   {
     Renderer([]{ return text(Welcome_Waffle);}),
     Container::Vertical(
-      {
-        Container::Horizontal({Renderer([]{ return text("Keymap") | vcenter | size(WIDTH, EQUAL, 15) ; }), Dropdown(&keymaps, &keymap_selected) | size(WIDTH, EQUAL, 25)}),
-        Container::Horizontal({Renderer([]{ return text("Locale") | vcenter | size(WIDTH, EQUAL, 15); }), Dropdown(&locales, &locale_selected) | size(WIDTH, EQUAL, 25)}),
-        Container::Horizontal({Renderer([]{ return text("Timezone") | vcenter | size(WIDTH, EQUAL, 15); }), Dropdown(&timezones, &timezone_selected) | size(WIDTH, EQUAL, 25)})
-      }),
-    Radiobox(&tab_2_entries, &tab_2_selected),
-    Radiobox(&tab_3_entries, &tab_3_selected)
+    {
+      Container::Horizontal({Renderer([]{ return text("Keymap") | vcenter | size(WIDTH, EQUAL, 15) ; }), Dropdown(&keymaps, &keymap_selected) | size(WIDTH, EQUAL, 25)}),
+      Container::Horizontal({Renderer([]{ return text("Locale") | vcenter | size(WIDTH, EQUAL, 15); }), Dropdown(&locales, &locale_selected) | size(WIDTH, EQUAL, 25)}),
+      Container::Horizontal({Renderer([]{ return text("Timezone") | vcenter | size(WIDTH, EQUAL, 15); }), Dropdown(&timezones, &timezone_selected) | size(WIDTH, EQUAL, 25)})
+    }),
+    Container::Vertical(
+    {
+      mounts
+    }),
+    std::make_shared<ComponentBase>(),
+    std::make_shared<ComponentBase>()
   },
   &tab_selected);
 
@@ -63,18 +125,37 @@ int main (int argc, char ** argv)
     tab_container,
   });
 
+  
+  auto screen = ScreenInteractive::Fullscreen();
+
+
   auto renderer = Renderer(container, [&]
   {
     return vbox(
             {
+              text(debug),
               tab_toggle->Render(),
               separator(),
               tab_container->Render(),
-            }) |
-            border;
+            }) | border;
   });
   
-  auto screen = ScreenInteractive::Fullscreen();
+
+  renderer |= CatchEvent([&](Event evt)
+  {
+    static int n_q{0};
+    
+    n_q = (evt == Event::q ? n_q+1 : 0);
+
+    if (n_q == 2)
+    {      
+      screen.Exit();
+      return true;
+    }
+    else
+      return false;
+  });
+
   screen.Loop(renderer);
   
   return 0;
